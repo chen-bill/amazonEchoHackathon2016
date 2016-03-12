5bbd67842d9c3a72fcb22ca192d7fbc8684b7290
Bill Chen <chen.bill96@gmail.com>
2016-03-12 14:15:40 -0500

    initial commit

---
+++ utterances.js
@@ -0,0 +1,79 @@
+WhatPostIntent {Pronoun} {WhatKey} is {WhatValue}
+WhatPostIntent {WhatKey} is {WhatValue}
+
+WhatGetIntent What is {Pronoun} {WhatKey}
+WhatGetIntent What are {Pronoun} {WhatKey}
+
+WherePostIntent My {WhereKey} is {WhereValue} 
+WherePostIntent I can find my  {WhereKey} {WhereValue} 
+WherePostIntent I found my {WhereKey} {WhereValue}
+WherePostIntent My {WhereKey} is located {WhereValue}
+WherePostIntent My {WhereKey} is located {WhereValue}
+WherePostIntent One can find {WhereKey} {WhereValue}
+WherePostIntent We can find {WhereKey} {WhereValue}
+WherePostIntent They can find {WhereKey} {WhereValue}
+WherePostIntent I would find {WhereKey} {WhereValue}
+WherePostIntent {WhereKey} can be found {WhereValue}
+WherePostIntent {WhereKey} can be found {WhereValue}
+WherePostIntent I placed my {WhereKey} {WhereValue}
+WherePostIntent I am placing my {WhereKey} {WhereValue} 
+WherePostIntent I put my {WhereKey} {WhereValue}
+WherePostIntent I am putting my {WhereKey} {WhereValue}
+WherePostIntent I am putting {WhereKey} {WhereValue}
+WherePostIntent I am putting {WhereKey} at {WhereValue}
+WherePostIntent {WhereKey} got to {WhereValue}
+WherePostIntent My {WhereKey} has gone to {WhereValue}
+WherePostIntent {WhereKey} has gone to {WhereValue}
+WherePostIntent {WhereKey} went to {WhereValue}
+WherePostIntent {WhereKey} is now {WhereValue}
+WherePostIntent {WhereKey} was {WhereValue}
+WherePostIntent {WhereKey} was {WhereValue} {WhereTime}
+WherePostIntent {WhereKey} was {WhereValue} {WhereTime} 
+WherePostIntent {WhereKey} was {WhereValue} {WhereDate}
+WherePostIntent {WhereKey} was {WhereValue} {WhereDate} 
+WherePostIntent My {WhereKey} was {WhereValue} {WhereTime}
+WherePostIntent My {WhereKey} was {WhereValue} {WhereDate}
+WherePostIntent I think {WhereKey} is {WhereValue}
+WherePostIntent I think my {WhereKey} is {WhereValue}
+WherePostIntent I think {WhereKey} is {WhereValue}
+WherePostIntent I think my {WhereKey} is {WhereValue}
+WherePostIntent {WhereKey} location is {WhereValue}
+WherePostIntent {WhereKey} is {WhereValue}
+WherePostIntent I suppose {WhereKey} is {WhereValue}
+WherePostIntent I suppose I can find {WhereKey} {WhereValue}
+WherePostIntent Maybe I can find {WhereKey} {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} is {WhereValue} 
+WherePostIntent I can find {Pronoun} {WhereKey} {WhereValue} 
+WherePostIntent I found {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} is located {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} is located {WhereValue}
+WherePostIntent One can find {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent We can find {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent They can find {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} is on {Pronoun} {WhereValue}
+WherePostIntent I would find {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} can be found {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} can be found {WhereValue}
+WherePostIntent I placed {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent I am placing {Pronoun} {WhereKey} {WhereValue} 
+WherePostIntent I put {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent I am putting {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent I am putting {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} got to {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} has gone to {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} went to {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} is now {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue} {WhereTime}
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue} {WhereTime} 
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue} {WhereDate}
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue} {WhereDate} 
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue} {WhereTime}
+WherePostIntent {Pronoun} {WhereKey} was {WhereValue} {WhereDate}
+WherePostIntent I think {Pronoun} {WhereKey} is {WhereValue}
+WherePostIntent I think {Pronoun} {WhereKey} is {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} location is {WhereValue}
+WherePostIntent {Pronoun} {WhereKey} is {WhereValue}
+WherePostIntent I suppose {Pronoun} {WhereKey} is {WhereValue}
+WherePostIntent I suppose I can find {Pronoun} {WhereKey} {WhereValue}
+WherePostIntent Maybe I can find {Pronoun} {WhereKey} {WhereValue}
